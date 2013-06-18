f0456d1a51eca03b36be2b879dafc7ce1927c781
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-06-18 22:40:35 +0200

    More tests for object operations.

--- tests/chronicle/006-object.js
+++ tests/chronicle/006-object.js
@@ -6,6 +6,7 @@ var errors = root.Substance.errors;
 var Chronicle = root.Substance.Chronicle;
 var ObjectOperation = Chronicle.OT.ObjectOperation;
 var TextOperation = Chronicle.OT.TextOperation;
+var ArrayOperation = Chronicle.OT.ArrayOperation;
 
 function testTransform(a, b, input, expected) {
   var t = ObjectOperation.transform(a, b);
@@ -80,6 +81,29 @@ var ObjectOperationTest = function() {
       });
     },
 
+    "Apply: update (text)", function() {
+      var path = ["a"];
+      var op = ObjectOperation.Update(path, TextOperation.fromOT("bla", [2, -1, "upp"]));
+      var expected = {a: "blupp"};
+
+      var obj = {a: "bla"};
+      op.apply(obj);
+
+      assert.isObjectEqual(expected, obj);
+    },
+
+    "Apply: update (array)", function() {
+      var path = ["a"];
+      var val = [1,2,3,4,5];
+      var op = ObjectOperation.Update(path, ArrayOperation.Sequence([2, ['-', 3], 2, ['+', 6]]));
+      var expected = {a: [1,2,4,5,6]};
+
+      var obj = {a: val.slice(0)};
+      op.apply(obj);
+
+      assert.isObjectEqual(expected, obj);
+    },
+
     // Conflict cases
     "Transformation: create/create (conflict)", function() {
       var path = ["a"];
@@ -92,7 +116,7 @@ var ObjectOperationTest = function() {
       assert.isTrue(ObjectOperation.hasConflict(a, b));
 
       assert.exception(errors.ChronicleError, function() {
-        t = ObjectOperation.transform(a, b);
+        ObjectOperation.transform(a, b);
       });
     },
 
@@ -126,13 +150,13 @@ var ObjectOperationTest = function() {
       t = ObjectOperation.transform(a, b);
       obj = t[1].apply(a.apply({a: "bla"}));
       assert.isObjectEqual(expected1, obj);
-      obj = t[0].apply(b.apply({}))
+      obj = t[0].apply(b.apply({}));
       assert.isObjectEqual(expected1, obj);
 
       t = ObjectOperation.transform(b, a);
       obj = t[1].apply(b.apply({}));
       assert.isObjectEqual(expected2, obj);
-      obj = t[0].apply(a.apply({a: "bla"}))
+      obj = t[0].apply(a.apply({a: "bla"}));
       assert.isObjectEqual(expected2, obj);
 
     },
@@ -159,7 +183,7 @@ var ObjectOperationTest = function() {
 
       assert.isTrue(ObjectOperation.hasConflict(a, b));
       assert.exception(errors.ChronicleError, function() {
-        t = ObjectOperation.transform(a, b);
+        ObjectOperation.transform(a, b);
       });
     },
 
@@ -177,6 +201,72 @@ var ObjectOperationTest = function() {
       testTransform(a, b, input, expected1);
       testTransform(b, a, input, expected2);
     },
+
+    "Transformation: delete/set (conflict)", function() {
+      var path = ["a"];
+      var a = ObjectOperation.Delete(path, "bla");
+      var b = ObjectOperation.Set(path, "bla", "blupp");
+
+      var input = {a : "bla"};
+      var expected1 = {a: "blupp"};
+      var expected2 = {};
+
+      assert.isTrue(ObjectOperation.hasConflict(a, b));
+
+      testTransform(a, b, input, expected1);
+      testTransform(b, a, input, expected2);
+    },
+
+    "Transformation: create/set (conflict)", function() {
+      var path = ["a"];
+      var a = ObjectOperation.Create(path, "foo");
+      var b = ObjectOperation.Set(path, "bla", "blupp");
+
+      var expected1 = {a: "blupp"};
+      var expected2 = {a: "foo"};
+
+      assert.isTrue(ObjectOperation.hasConflict(a, b));
+
+      var t = ObjectOperation.transform(a, b);
+      var obj = t[1].apply(a.apply({}));
+      assert.isObjectEqual(expected1, obj);
+      obj = t[0].apply(b.apply({a: "bla"}));
+      assert.isObjectEqual(expected1, obj);
+
+      t = ObjectOperation.transform(b, a);
+      obj = t[1].apply(b.apply({a: "bla"}));
+      assert.isObjectEqual(expected2, obj);
+      obj = t[0].apply(a.apply({}));
+      assert.isObjectEqual(expected2, obj);
+    },
+
+    "Transformation: update/set (conflict)", function() {
+      var path = ["a"];
+      var a = ObjectOperation.Update(path, TextOperation.fromOT("bla", [2, -1, "upp"]));
+      var b = ObjectOperation.Set(path, "bla", "blupp");
+
+      assert.isTrue(ObjectOperation.hasConflict(a, b));
+
+      assert.exception(errors.ChronicleError, function() {
+        ObjectOperation.transform(a,b);
+      });
+    },
+
+    "Transformation: set/set (conflict)", function() {
+      var path = ["a"];
+      var a = ObjectOperation.Set(path, "bla", "blapp");
+      var b = ObjectOperation.Set(path, "bla", "blupp");
+
+      var input = {a : "bla"};
+      var expected1 = {a: "blupp"};
+      var expected2 = {a: "blapp"};
+
+      assert.isTrue(ObjectOperation.hasConflict(a, b));
+
+      testTransform(a, b, input, expected1);
+      testTransform(b, a, input, expected2);
+    },
+
   ];
 
 };
